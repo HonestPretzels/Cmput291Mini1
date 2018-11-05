17364e9a451f362814fac75c85e6bb3a5fe369c1
Jolene Poulin <jcpoulin@ualberta.ca>
2018-11-05 13:30:12 -0700

    Can only delete your own requests now

--- deleteRequest.py
+++ deleteRequest.py
@@ -9,7 +9,7 @@ def delete_request(username, database):
     c.execute("SELECT * FROM requests WHERE email = :u_name;", { "u_name":username })
     rides = c.fetchall()
 
-    try:
+    if(len(rides) > 0):
         for ride in rides:
             print(ride)
 
@@ -26,10 +26,34 @@ def delete_request(username, database):
         if (r_number == 'q'):
             return
 
+        # Make sure the user owns the selected request
+        c.execute("SELECT email FROM requests WHERE rid = :r_no;", { "r_no":r_number})
+        name = c.fetchone()
+
+        while (name[0] != username):
+            print("You do not own this request. Please select a request that you have made to delete it.")
+            r_number = input("Please enter the id of the request you would like to delete or 'q' to quit: ")
+
+            # Validate input
+            valid_number = re.match('^[A-Za-z0-9_]*$', r_number)
+            while (not valid_number):
+                print("Please enter a valid id or 'q' to quit: ")
+                r_number = input()
+                valid_number = re.match('^[A-Za-z0-9_]*$', r_number)
+
+            # End this function
+            if (r_number == 'q'):
+                return
+
+            # Make sure the user owns the selected request
+            c.execute("SELECT email FROM requests WHERE rid = :r_no;", { "r_no":r_number})
+            name = c.fetchone()
+
         # Delete request
         c.execute("DELETE FROM requests WHERE rid = :r_no;", { "r_no":r_number})
         conn.commit()
-    except:
+        print("Request number " + r_number + " has been deleted")
+    else:
         print("No requests to show!")
 
     conn.close()
